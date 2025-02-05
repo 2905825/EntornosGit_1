273ca4754df8c96611628f2574889cea2b6dbae4
antonio <an@2006.com>
2025-02-05 13:17:52 +0100

    Aquí va la clase usuario

---
+++ Usuario.java
@@ -0,0 +1,10 @@
+
+public class Usuario {
+    public String nombre;
+    public String apellidos;
+    public String email;
+    public String direccion;
+    public String ubicacion;
+    public int teléfono;
+    
+}
